1291fd1e14ee5b124c1e3f82e1b899bd4cde5297
ikhda <ikhdamuhammad@gmail.com>
2019-04-09 00:28:44 +0700

    initial commit

--- vidly/index.js
+++ vidly/index.js
@@ -1,5 +1,7 @@
 const Joi = require("joi");
 const logger = require("./logger");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 const express = require("express");
 const app = express();
@@ -7,6 +9,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(helmet());
+app.use(morgan("tiny"));
 
 app.use(logger);
 
