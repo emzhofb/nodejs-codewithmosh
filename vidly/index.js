29d2a3f34d1eb070da243c2b62125cb8754a5460
ikhda <ikhdamuhammad@gmail.com>
2019-04-09 00:11:39 +0700

    initial commit

--- vidly/index.js
+++ vidly/index.js
@@ -1,9 +1,11 @@
 const Joi = require("joi");
+const logger = require("./logger");
 
 const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use(logger);
 
 const genres = [
   { id: 1, name: "Action" },
