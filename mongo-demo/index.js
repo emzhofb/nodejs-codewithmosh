9d5a6c4a8fe5dcda4c6530f3413082b2a7507b43
ikhda <ikhdamuhammad@gmail.com>
2019-04-09 23:32:53 +0700

    add exercise 1

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -32,11 +32,15 @@ async function createCourses() {
 }
 
 async function getCourses() {
+  const pageNumber = 2;
+  const pageSize = 10;
+
   const courses = await Course.find({
     author: "Mosh",
     isPublish: true
   })
-    .limit(10)
+    .skip((pageNumber - 1) * pageSize)
+    .limit(pageSize)
     .sort({
       name: 1
     })
