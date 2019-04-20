7588922abd88d4caeec7f74866899c58a99507d4
ikhda <ikhdamuhammad@gmail.com>
2019-04-20 22:49:02 +0700

    exercise

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -129,6 +129,7 @@ describe("applyDiscount", () => {
 
 describe("notifyCustomer", () => {
   it("should send an email to the customer", () => {
+    /*
     db.getCustomerSync = function(customerId) {
       return { email: "a" };
     };
@@ -141,5 +142,17 @@ describe("notifyCustomer", () => {
     lib.notifyCustomer({ customerId: 1 });
 
     expect(mailSent).toBe(true);
+    */
+
+    // jest mock function
+
+    db.getCustomerSync = jest.fn().mockReturnValue({ email: "a" });
+    mail.send = jest.fn();
+
+    lib.notifyCustomer({ customerId: 1 });
+
+    expect(mail.send).toHaveBeenCalled();
+    expect(mail.send.mock.calls[0][0]).toBe("a");
+    expect(mail.send.mock.calls[0][1]).toMatch(/order/);
   });
 });
