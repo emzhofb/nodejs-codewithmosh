793785b04360922b971645047dbf3bded9796bbe
ikhda <ikhdamuhammad@gmail.com>
2019-04-20 12:23:51 +0700

    jest watchAll

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -87,3 +87,19 @@ describe("getProduct", () => {
     expect(result).toHaveProperty("id", 1); // first argument is key, the second is value
   });
 });
+
+describe("registerUser", () => {
+  it("should throw is username is falsy", () => {
+    const args = [null, undefined, NaN, "", 0, false];
+    args.forEach(a => {
+      expect(() => {
+        lib.registerUser(a);
+      }).toThrow();
+    });
+  });
+  it("should return a user object if valid username is passed", () => {
+    const result = lib.registerUser("mosh");
+    expect(result).toMatchObject({ username: "mosh" });
+    expect(result.id).toBeGreaterThan(0);
+  });
+});
