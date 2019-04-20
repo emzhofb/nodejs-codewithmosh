98a3f2486e3bd31dcb338cfde01f776d98d60224
ikhda <ikhdamuhammad@gmail.com>
2019-04-21 00:24:16 +0700

    grouping test

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,22 +1,38 @@
 const lib = require("../lib");
 
-test("absolut - should return a positive number if input is positive", () => {
-  // our test goes here
-  const result = lib.absolute(1);
-  // expect
-  expect(result).toBe(1);
+// grouping testing
+describe("absolute", () => {
+  it("should return a positive number if input is positive", () => {
+    // our test goes here
+    const result = lib.absolute(1);
+    // expect
+    expect(result).toBe(1);
+  });
+
+  it("should return a positive number if input is negative", () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+  });
+
+  it("should return 0 if input is 0", () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+  });
 });
 
-test("absolut - should return a positive number if input is negative", () => {
-  // our test goes here
-  const result = lib.absolute(-1);
-  // expect
-  expect(result).toBe(1);
-});
+// test("absolut - should return a positive number if input is positive", () => {
+//   // our test goes here
+//   const result = lib.absolute(1);
+//   // expect
+//   expect(result).toBe(1);
+// });
 
-test("absolut - should return 0 if input is 0", () => {
-  // our test goes here
-  const result = lib.absolute(0);
-  // expect
-  expect(result).toBe(0);
-});
+// test("absolut - should return a positive number if input is negative", () => {
+//   const result = lib.absolute(-1);
+//   expect(result).toBe(1);
+// });
+
+// test("absolut - should return 0 if input is 0", () => {
+//   const result = lib.absolute(0);
+//   expect(result).toBe(0);
+// });
