66233a962eb803f230319ce4454bb5be9259be8e
ikhda <ikhdamuhammad@gmail.com>
2019-04-21 00:37:44 +0700

    string testing

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -20,19 +20,30 @@ describe("absolute", () => {
   });
 });
 
-// test("absolut - should return a positive number if input is positive", () => {
-//   // our test goes here
-//   const result = lib.absolute(1);
-//   // expect
-//   expect(result).toBe(1);
-// });
+/*
+test("absolut - should return a positive number if input is positive", () => {
+  // our test goes here
+  const result = lib.absolute(1);
+  // expect
+  expect(result).toBe(1);
+});
 
-// test("absolut - should return a positive number if input is negative", () => {
-//   const result = lib.absolute(-1);
-//   expect(result).toBe(1);
-// });
+test("absolut - should return a positive number if input is negative", () => {
+  const result = lib.absolute(-1);
+  expect(result).toBe(1);
+});
 
-// test("absolut - should return 0 if input is 0", () => {
-//   const result = lib.absolute(0);
-//   expect(result).toBe(0);
-// });
+test("absolut - should return 0 if input is 0", () => {
+  const result = lib.absolute(0);
+  expect(result).toBe(0);
+});
+*/
+
+describe("greet", () => {
+  it("should return the greeting message", () => {
+    const result = lib.greet("Mosh");
+    expect(result).toMatch(/Mosh/);
+    // or use
+    expect(result).toContain("Mosh");
+  });
+});
